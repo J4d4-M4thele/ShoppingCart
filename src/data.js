edcedb9c40ff0b5be61c03e55aab0c3fd2cc1010
J4d4-M4thele <jadamaadimo.mathele@gmail.com>
2023-08-23 16:56:17 +0200

    first commit

---
+++ src/data.js
@@ -0,0 +1,24 @@
+const data = {
+    products: [
+        {
+            id: '1',
+            name: 'Sunglasses',
+            price: 75,
+            image:'https://picsum.photos/id/180/2400/1600',
+        },
+        {
+            id: '1',
+            name: 'Tops',
+            price: 100,
+            image:'https://picsum.photos/id/111/4400/2656',
+        },
+        {
+            id: '1',
+            name: 'Low-rise Jeans',
+            price: 250,
+            image:'https://picsum.photos/id/21/3008/2008',
+        }
+    ]
+};
+
+export default data;
