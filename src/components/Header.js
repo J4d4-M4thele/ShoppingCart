dd864468f2f7e6a94178f6b6d35636c78e5793dc
J4d4-M4thele <jadamaadimo.mathele@gmail.com>
2023-08-24 10:14:27 +0200

    update

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,24 @@
 import React from 'react';
 
 export default function Header(props) {
-    return (
-        <header className='row block center'>
-            <div>
-                <a href='#/'>
-                    <h1> Small Shopping Cart</h1>
-                </a>
-            </div>
-            <div>
-                <a href='#/cart'>Cart</a> <a href='#/signin'>SignIn</a>
-            </div>
-        </header>
-    )
+  return (
+    <header className="block row center">
+      <div>
+        <a href="#/">
+          <h1>Small Shopping Cart</h1>
+        </a>
+      </div>
+      <div>
+        <a href="#/cart">
+          Cart{' '}
+          {props.countCartItems ? (
+            <button className="badge">{props.countCartItems}</button>
+          ) : (
+            ''
+          )}
+        </a>{' '}
+        <a href="#/signin"> SignIn</a>
+      </div>
+    </header>
+  );
 }
