09a0d279506f507701e09fb43fa80a78e9a89141
J4d4-M4thele <jadamaadimo.mathele@gmail.com>
2023-08-24 09:32:46 +0200

    update

--- src/App.js
+++ src/App.js
@@ -5,27 +5,47 @@ import data from './data';
 import { useState } from 'react';
 
 function App() {
-  //product info comes from data
+  //get product info from data
   const { products } = data;
-  //defining state
+
+  //set initial state
   const [cartItems, setCartItems] = useState([]);
+  
+  //event listeners
   const onAdd = (product) => {
-    const exist = cartItems.find(x => x.id === product.id);
+    const exist = cartItems.find((x) => x.id === product.id);
     if (exist) {
-      setCartItems(cartItems.map(x =>
-        x.id === product.id ? { ...exist, qty: exist.qty + 1 } : x
-      )
+      setCartItems(
+        cartItems.map((x) =>
+          x.id === product.id ? { ...exist, qty: exist.qty + 1 } : x
+        )
       );
-    }else{
-      setCartItems([...cartItems, {...product,qty: 1}]);
+    } else {
+      setCartItems([...cartItems, { ...product, qty: 1 }]);
+    }
+  };
+  const onRemove = (product) => {
+    const exist = cartItems.find((x) => x.id === product.id);
+    if (exist.qty === 1) {
+      setCartItems(cartItems.filter((x) => x.id !== product.id));
+    } else {
+      setCartItems(
+        cartItems.map((x) =>
+          x.id === product.id ? { ...exist, qty: exist.qty - 1 } : x
+        )
+      );
     }
   };
   return (
     <div className="App">
-      <Header></Header>
-      <div className='row'>
-        <Main onAdd={onAdd} products={products}></Main>
-        <Basket onAdd={onAdd} cartItems={cartItems}></Basket>
+      <Header countCartItems={cartItems.length}></Header>
+      <div className="row">
+        <Main products={products} onAdd={onAdd}></Main>
+        <Basket
+          cartItems={cartItems}
+          onAdd={onAdd}
+          onRemove={onRemove}
+        ></Basket>
       </div>
     </div>
   );
--- src/components/Basket.js
+++ src/components/Basket.js
@@ -1,17 +1,23 @@
 import React from 'react';
 
 export default function Basket(props) {
-  const {cartItems, onAdd} = props;
-    return <aside className='block col-1'>
-      <h2>Cart Items</h2>
-      <div>
-        {cartItems.length === 0 && <div>Cart Is Empty</div>}
-        {cartItems.map((item) => (
-          <div key={item.id} className='row'>
-            <div>{item.name}</div>
-            <div></div>
+  const { cartItems, onAdd, onRemove } = props;
+  return <aside className='block col-1'>
+    <h2>Cart Items</h2>
+    <div>
+      {cartItems.length === 0 && <div>Cart Is Empty</div>}
+      {cartItems.map((item) => (
+        <div key={item.id} className='row'>
+          <div className='col-2'>{item.name}</div>
+          <div className='col-2'>
+            <button onClick={() => onAdd(item)} className='add'>+</button>
+            <button onClick={() => onRemove(item)} className='remove'>-</button>
           </div>
-        ))}
-      </div>
-    </aside>;
+          <div className='col-2 text-right'>
+            {item.qty} x R{item.price.toFixed(2)}
+          </div>
+        </div>
+      ))}
+    </div>
+  </aside>;
 }
