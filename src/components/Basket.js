117269877494ddd6799f6f49f477ac46d7c22662
J4d4-M4thele <jadamaadimo.mathele@gmail.com>
2023-08-24 09:08:09 +0200

    update

--- src/components/Basket.js
+++ src/components/Basket.js
@@ -1,7 +1,17 @@
 import React from 'react';
 
 export default function Basket(props) {
+  const {cartItems, onAdd} = props;
     return <aside className='block col-1'>
       <h2>Cart Items</h2>
+      <div>
+        {cartItems.length === 0 && <div>Cart Is Empty</div>}
+        {cartItems.map((item) => (
+          <div key={item.id} className='row'>
+            <div>{item.name}</div>
+            <div></div>
+          </div>
+        ))}
+      </div>
     </aside>;
 }
--- src/data.js
+++ src/data.js
@@ -4,19 +4,19 @@ const data = {
             id: '1',
             name: 'Sunglasses',
             price: 75,
-            image:'https://picsum.photos/id/180/2400/1600',
+            image:'https://images-na.ssl-images-amazon.com/images/I/71GE6XJxYIL.jpg',
         },
         {
             id: '1',
-            name: 'Tops',
+            name: 'Y2k Top',
             price: 100,
-            image:'https://picsum.photos/id/111/4400/2656',
+            image:'https://i.etsystatic.com/29141266/r/il/a3a22c/3142284035/il_fullxfull.3142284035_jmfh.jpg',
         },
         {
             id: '1',
-            name: 'Low-rise Jeans',
+            name: 'Shoulderbag',
             price: 250,
-            image:'https://picsum.photos/id/21/3008/2008',
+            image:'https://i.pinimg.com/736x/23/16/23/231623aad564974d0b6dbf162da8c381.jpg',
         }
     ]
 };
